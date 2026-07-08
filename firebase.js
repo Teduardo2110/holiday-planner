ae8a38f01896842343d7083aba1705a085b5ac7f
Teduardo2110 <Ejmartin2110@outlook.com>
2026-07-08 15:50:55 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,15 +3,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBQcdYkLKIvYIkPG4N-Dr13hUKGcdKDvPI",
   authDomain: "holiday-planner-bf92f.firebaseapp.com",
@@ -22,10 +14,6 @@ const firebaseConfig = {
   measurementId: "G-6GVHS07HV9"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-};
 
 const app = initializeApp(firebaseConfig);
 
